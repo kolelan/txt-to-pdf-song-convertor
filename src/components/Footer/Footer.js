ae903769536ec596f16b442707f64c483d64e41c
gstepenko <g.stepenko@rusbitech.ru>
2025-09-02 09:48:01 +0300

    Компонент Footer

---
+++ src/components/Footer/Footer.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import './Footer.css';
+
+const Footer = () => {
+    return (
+        <div className="footer">
+            <div className="footer-header">
+                <div className="footer-message">
+                    Приятных занятий 🎸
+                </div>
+            </div>
+        </div>
+    );
+};
+
+export default Footer;
