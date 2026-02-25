04ab08227c55af007d0133ff244c4564886ef98f
gstepenko <g.stepenko@rusbitech.ru>
2026-02-25 10:40:59 +0300

    Добавлены все септаккорды для всех тональностей
    Добавлены следующие типы септаккордов:
    - Major 7th (maj7): мажорные септаккорды (1-3-5-7)
    - Minor 7th (m7): минорные септаккорды (1-b3-5-b7)
    - Dominant 7th (7): доминантсептаккорды (1-3-5-b7)
    - Diminished 7th (dim7): уменьшенные септаккорды (1-b3-b5-bb7)
    - Half-diminished 7th (m7b5): полууменьшенные септаккорды (1-b3-b5-b7)
    - Augmented 7th (aug7): увеличенные септаккорды (1-3-#5-b7)
    
    Все септаккорды добавлены для всех тональностей (C, C#, Cb, Db, D, D#, Eb, E, E#, F, Fb, F#, Gb, G, G#, Ab, A, A#, Bb, B, B#).
    
    Всего добавлено 120 новых аккордов (6 типов × 20 тональностей).
    Все аккорды используют ноты в 3-й октаве, как и обычные мажорные/минорные аккорды.
    
    Также улучшена огибающая ADSR для более плавного и тянучего звука:
    - Убрана быстрая атака (0.001 сек - почти без щелчков)
    - Добавлен sustain (0.8) для равномерного звука на протяжении большей части длительности
    - Плавное затухание только в конце (0.05 сек)
    
    Добавлены настройки синтезатора (synthSettings) с возможностью изменения параметров.

--- src/utils/midiConverter.js
+++ src/utils/midiConverter.js
@@ -11,7 +11,11 @@ export const midiNotes = {
     'C8': 108, 'C#8': 109, 'Db8': 109, 'D8': 110, 'D#8': 111, 'Eb8': 111, 'E8': 112, 'F8': 113, 'F#8': 114, 'Gb8': 114, 'G8': 115, 'G#8': 116, 'Ab8': 116, 'A8': 117, 'A#8': 118, 'Bb8': 118, 'B8': 119
 };
 
-// Аккорды по умолчанию (все мажорные и минорные в 3-й октаве)
+// Аккорды по умолчанию (мажорные, минорные и все септаккорды в 3-й октаве)
+// Включает: мажорные (C, C#, D, ...), минорные (Cm, C#m, Dm, ...),
+// Major 7th (Cmaj7, C#maj7, ...), Minor 7th (Cm7, C#m7, ...),
+// Dominant 7th (C7, C#7, ...), Diminished 7th (Cdim7, C#dim7, ...),
+// Half-diminished 7th (Cm7b5, C#m7b5, ...), Augmented 7th (Caug7, C#aug7, ...)
 export const defaultChordNotes = {
     'C':  { notes: ['C3', 'E3', 'G3'], name: 'До мажор' },
     'C#': { notes: ['C#3', 'F3', 'G#3'], name: 'До-диез мажор' },
@@ -54,7 +58,145 @@ export const defaultChordNotes = {
     'A#m': { notes: ['A#3', 'C#4', 'F4'], name: 'Ля-диез минор' },
     'Bbm': { notes: ['Bb3', 'Db4', 'F4'], name: 'Си-бемоль минор' },
     'Bm':  { notes: ['B3', 'D4', 'F#4'], name: 'Си минор' },
-    'B#m': { notes: ['C4', 'Eb4', 'G4'], name: 'Си-диез минор' } // Энгармонически C минор
+    'B#m': { notes: ['C4', 'Eb4', 'G4'], name: 'Си-диез минор' }, // Энгармонически C минор
+    
+    // Major 7th (maj7, M7) - мажорные септаккорды: 1-3-5-7
+    'Cmaj7':  { notes: ['C3', 'E3', 'G3', 'B3'], name: 'До мажор септаккорд' },
+    'C#maj7': { notes: ['C#3', 'F3', 'G#3', 'C4'], name: 'До-диез мажор септаккорд' },
+    'Cbmaj7': { notes: ['B2', 'D#3', 'F#3', 'A#3'], name: 'До-бемоль мажор септаккорд' },
+    'Dbmaj7': { notes: ['Db3', 'F3', 'Ab3', 'C4'], name: 'Ре-бемоль мажор септаккорд' },
+    'Dmaj7':  { notes: ['D3', 'F#3', 'A3', 'C#4'], name: 'Ре мажор септаккорд' },
+    'D#maj7': { notes: ['D#3', 'G3', 'A#3', 'D4'], name: 'Ре-диез мажор септаккорд' },
+    'Ebmaj7': { notes: ['Eb3', 'G3', 'Bb3', 'D4'], name: 'Ми-бемоль мажор септаккорд' },
+    'Emaj7':  { notes: ['E3', 'G#3', 'B3', 'D#4'], name: 'Ми мажор септаккорд' },
+    'E#maj7': { notes: ['F3', 'A3', 'C4', 'E4'], name: 'Ми-диез мажор септаккорд' },
+    'Fmaj7':  { notes: ['F3', 'A3', 'C4', 'E4'], name: 'Фа мажор септаккорд' },
+    'Fbmaj7': { notes: ['E3', 'G#3', 'B3', 'D#4'], name: 'Фа-бемоль мажор септаккорд' },
+    'F#maj7': { notes: ['F#3', 'A#3', 'C#4', 'F4'], name: 'Фа-диез мажор септаккорд' },
+    'Gbmaj7': { notes: ['Gb3', 'Bb3', 'Db4', 'F4'], name: 'Соль-бемоль мажор септаккорд' },
+    'Gmaj7':  { notes: ['G3', 'B3', 'D4', 'F#4'], name: 'Соль мажор септаккорд' },
+    'G#maj7': { notes: ['G#3', 'C4', 'D#4', 'G4'], name: 'Соль-диез мажор септаккорд' },
+    'Abmaj7': { notes: ['Ab3', 'C4', 'Eb4', 'G4'], name: 'Ля-бемоль мажор септаккорд' },
+    'Amaj7':  { notes: ['A3', 'C#4', 'E4', 'G#4'], name: 'Ля мажор септаккорд' },
+    'A#maj7': { notes: ['A#3', 'D4', 'F4', 'A4'], name: 'Ля-диез мажор септаккорд' },
+    'Bbmaj7': { notes: ['Bb3', 'D4', 'F4', 'A4'], name: 'Си-бемоль мажор септаккорд' },
+    'Bmaj7':  { notes: ['B3', 'D#4', 'F#4', 'A#4'], name: 'Си мажор септаккорд' },
+    'B#maj7': { notes: ['C4', 'E4', 'G4', 'B4'], name: 'Си-диез мажор септаккорд' },
+    
+    // Minor 7th (m7) - минорные септаккорды: 1-b3-5-b7
+    'Cm7':  { notes: ['C3', 'Eb3', 'G3', 'Bb3'], name: 'До минор септаккорд' },
+    'C#m7': { notes: ['C#3', 'E3', 'G#3', 'B3'], name: 'До-диез минор септаккорд' },
+    'Cbm7': { notes: ['B2', 'D3', 'F#3', 'A3'], name: 'До-бемоль минор септаккорд' },
+    'Dbm7': { notes: ['Db3', 'E3', 'Ab3', 'C4'], name: 'Ре-бемоль минор септаккорд' },
+    'Dm7':  { notes: ['D3', 'F3', 'A3', 'C4'], name: 'Ре минор септаккорд' },
+    'D#m7': { notes: ['D#3', 'F#3', 'A#3', 'C#4'], name: 'Ре-диез минор септаккорд' },
+    'Ebm7': { notes: ['Eb3', 'Gb3', 'Bb3', 'Db4'], name: 'Ми-бемоль минор септаккорд' },
+    'Em7':  { notes: ['E3', 'G3', 'B3', 'D4'], name: 'Ми минор септаккорд' },
+    'E#m7': { notes: ['F3', 'Ab3', 'C4', 'Eb4'], name: 'Ми-диез минор септаккорд' },
+    'Fm7':  { notes: ['F3', 'Ab3', 'C4', 'Eb4'], name: 'Фа минор септаккорд' },
+    'Fbm7': { notes: ['E3', 'G3', 'B3', 'D4'], name: 'Фа-бемоль минор септаккорд' },
+    'F#m7': { notes: ['F#3', 'A3', 'C#4', 'E4'], name: 'Фа-диез минор септаккорд' },
+    'Gbm7': { notes: ['Gb3', 'A3', 'Db4', 'F4'], name: 'Соль-бемоль минор септаккорд' },
+    'Gm7':  { notes: ['G3', 'Bb3', 'D4', 'F4'], name: 'Соль минор септаккорд' },
+    'G#m7': { notes: ['G#3', 'B3', 'D#4', 'G4'], name: 'Соль-диез минор септаккорд' },
+    'Abm7': { notes: ['Ab3', 'B3', 'Eb4', 'Gb4'], name: 'Ля-бемоль минор септаккорд' },
+    'Am7':  { notes: ['A3', 'C4', 'E4', 'G4'], name: 'Ля минор септаккорд' },
+    'A#m7': { notes: ['A#3', 'C#4', 'F4', 'A4'], name: 'Ля-диез минор септаккорд' },
+    'Bbm7': { notes: ['Bb3', 'Db4', 'F4', 'Ab4'], name: 'Си-бемоль минор септаккорд' },
+    'Bm7':  { notes: ['B3', 'D4', 'F#4', 'A4'], name: 'Си минор септаккорд' },
+    'B#m7': { notes: ['C4', 'Eb4', 'G4', 'Bb4'], name: 'Си-диез минор септаккорд' },
+    
+    // Dominant 7th (7) - доминантсептаккорды: 1-3-5-b7
+    'C7':  { notes: ['C3', 'E3', 'G3', 'Bb3'], name: 'До доминантсептаккорд' },
+    'C#7': { notes: ['C#3', 'F3', 'G#3', 'B3'], name: 'До-диез доминантсептаккорд' },
+    'Cb7': { notes: ['B2', 'D#3', 'F#3', 'A3'], name: 'До-бемоль доминантсептаккорд' },
+    'Db7': { notes: ['Db3', 'F3', 'Ab3', 'C4'], name: 'Ре-бемоль доминантсептаккорд' },
+    'D7':  { notes: ['D3', 'F#3', 'A3', 'C4'], name: 'Ре доминантсептаккорд' },
+    'D#7': { notes: ['D#3', 'G3', 'A#3', 'C#4'], name: 'Ре-диез доминантсептаккорд' },
+    'Eb7': { notes: ['Eb3', 'G3', 'Bb3', 'Db4'], name: 'Ми-бемоль доминантсептаккорд' },
+    'E7':  { notes: ['E3', 'G#3', 'B3', 'D4'], name: 'Ми доминантсептаккорд' },
+    'E#7': { notes: ['F3', 'A3', 'C4', 'Eb4'], name: 'Ми-диез доминантсептаккорд' },
+    'F7':  { notes: ['F3', 'A3', 'C4', 'Eb4'], name: 'Фа доминантсептаккорд' },
+    'Fb7': { notes: ['E3', 'G#3', 'B3', 'D4'], name: 'Фа-бемоль доминантсептаккорд' },
+    'F#7': { notes: ['F#3', 'A#3', 'C#4', 'E4'], name: 'Фа-диез доминантсептаккорд' },
+    'Gb7': { notes: ['Gb3', 'Bb3', 'Db4', 'F4'], name: 'Соль-бемоль доминантсептаккорд' },
+    'G7':  { notes: ['G3', 'B3', 'D4', 'F4'], name: 'Соль доминантсептаккорд' },
+    'G#7': { notes: ['G#3', 'C4', 'D#4', 'G4'], name: 'Соль-диез доминантсептаккорд' },
+    'Ab7': { notes: ['Ab3', 'C4', 'Eb4', 'Gb4'], name: 'Ля-бемоль доминантсептаккорд' },
+    'A7':  { notes: ['A3', 'C#4', 'E4', 'G4'], name: 'Ля доминантсептаккорд' },
+    'A#7': { notes: ['A#3', 'D4', 'F4', 'A4'], name: 'Ля-диез доминантсептаккорд' },
+    'Bb7': { notes: ['Bb3', 'D4', 'F4', 'Ab4'], name: 'Си-бемоль доминантсептаккорд' },
+    'B7':  { notes: ['B3', 'D#4', 'F#4', 'A4'], name: 'Си доминантсептаккорд' },
+    'B#7': { notes: ['C4', 'E4', 'G4', 'Bb4'], name: 'Си-диез доминантсептаккорд' },
+    
+    // Diminished 7th (dim7) - уменьшенные септаккорды: 1-b3-b5-bb7
+    'Cdim7':  { notes: ['C3', 'Eb3', 'Gb3', 'A3'], name: 'До уменьшенный септаккорд' },
+    'C#dim7': { notes: ['C#3', 'E3', 'G3', 'Bb3'], name: 'До-диез уменьшенный септаккорд' },
+    'Cbdim7': { notes: ['B2', 'D3', 'F3', 'Ab3'], name: 'До-бемоль уменьшенный септаккорд' },
+    'Dbdim7': { notes: ['Db3', 'E3', 'G3', 'Bb3'], name: 'Ре-бемоль уменьшенный септаккорд' },
+    'Ddim7':  { notes: ['D3', 'F3', 'Ab3', 'B3'], name: 'Ре уменьшенный септаккорд' },
+    'D#dim7': { notes: ['D#3', 'F#3', 'A3', 'C4'], name: 'Ре-диез уменьшенный септаккорд' },
+    'Ebdim7': { notes: ['Eb3', 'Gb3', 'A3', 'C4'], name: 'Ми-бемоль уменьшенный септаккорд' },
+    'Edim7':  { notes: ['E3', 'G3', 'Bb3', 'Db4'], name: 'Ми уменьшенный септаккорд' },
+    'E#dim7': { notes: ['F3', 'Ab3', 'B3', 'D4'], name: 'Ми-диез уменьшенный септаккорд' },
+    'Fdim7':  { notes: ['F3', 'Ab3', 'B3', 'D4'], name: 'Фа уменьшенный септаккорд' },
+    'Fbdim7': { notes: ['E3', 'G3', 'Bb3', 'Db4'], name: 'Фа-бемоль уменьшенный септаккорд' },
+    'F#dim7': { notes: ['F#3', 'A3', 'C4', 'Eb4'], name: 'Фа-диез уменьшенный септаккорд' },
+    'Gbdim7': { notes: ['Gb3', 'A3', 'C4', 'Eb4'], name: 'Соль-бемоль уменьшенный септаккорд' },
+    'Gdim7':  { notes: ['G3', 'Bb3', 'Db4', 'E4'], name: 'Соль уменьшенный септаккорд' },
+    'G#dim7': { notes: ['G#3', 'B3', 'D4', 'F4'], name: 'Соль-диез уменьшенный септаккорд' },
+    'Abdim7': { notes: ['Ab3', 'B3', 'D4', 'F4'], name: 'Ля-бемоль уменьшенный септаккорд' },
+    'Adim7':  { notes: ['A3', 'C4', 'Eb4', 'Gb4'], name: 'Ля уменьшенный септаккорд' },
+    'A#dim7': { notes: ['A#3', 'C#4', 'E4', 'G4'], name: 'Ля-диез уменьшенный септаккорд' },
+    'Bbdim7': { notes: ['Bb3', 'Db4', 'F4', 'Ab4'], name: 'Си-бемоль уменьшенный септаккорд' },
+    'Bdim7':  { notes: ['B3', 'D4', 'F4', 'Ab4'], name: 'Си уменьшенный септаккорд' },
+    'B#dim7': { notes: ['C4', 'Eb4', 'Gb4', 'A4'], name: 'Си-диез уменьшенный септаккорд' },
+    
+    // Half-diminished 7th (m7b5) - полууменьшенные септаккорды: 1-b3-b5-b7
+    'Cm7b5':  { notes: ['C3', 'Eb3', 'Gb3', 'Bb3'], name: 'До полууменьшенный септаккорд' },
+    'C#m7b5': { notes: ['C#3', 'E3', 'G3', 'B3'], name: 'До-диез полууменьшенный септаккорд' },
+    'Cbm7b5': { notes: ['B2', 'D3', 'F3', 'A3'], name: 'До-бемоль полууменьшенный септаккорд' },
+    'Dbm7b5': { notes: ['Db3', 'E3', 'G3', 'C4'], name: 'Ре-бемоль полууменьшенный септаккорд' },
+    'Dm7b5':  { notes: ['D3', 'F3', 'Ab3', 'C4'], name: 'Ре полууменьшенный септаккорд' },
+    'D#m7b5': { notes: ['D#3', 'F#3', 'A3', 'C#4'], name: 'Ре-диез полууменьшенный септаккорд' },
+    'Ebm7b5': { notes: ['Eb3', 'Gb3', 'A3', 'Db4'], name: 'Ми-бемоль полууменьшенный септаккорд' },
+    'Em7b5':  { notes: ['E3', 'G3', 'Bb3', 'D4'], name: 'Ми полууменьшенный септаккорд' },
+    'E#m7b5': { notes: ['F3', 'Ab3', 'B3', 'Eb4'], name: 'Ми-диез полууменьшенный септаккорд' },
+    'Fm7b5':  { notes: ['F3', 'Ab3', 'B3', 'Eb4'], name: 'Фа полууменьшенный септаккорд' },
+    'Fbm7b5': { notes: ['E3', 'G3', 'Bb3', 'D4'], name: 'Фа-бемоль полууменьшенный септаккорд' },
+    'F#m7b5': { notes: ['F#3', 'A3', 'C4', 'E4'], name: 'Фа-диез полууменьшенный септаккорд' },
+    'Gbm7b5': { notes: ['Gb3', 'A3', 'C4', 'F4'], name: 'Соль-бемоль полууменьшенный септаккорд' },
+    'Gm7b5':  { notes: ['G3', 'Bb3', 'Db4', 'F4'], name: 'Соль полууменьшенный септаккорд' },
+    'G#m7b5': { notes: ['G#3', 'B3', 'D4', 'G4'], name: 'Соль-диез полууменьшенный септаккорд' },
+    'Abm7b5': { notes: ['Ab3', 'B3', 'D4', 'Gb4'], name: 'Ля-бемоль полууменьшенный септаккорд' },
+    'Am7b5':  { notes: ['A3', 'C4', 'Eb4', 'G4'], name: 'Ля полууменьшенный септаккорд' },
+    'A#m7b5': { notes: ['A#3', 'C#4', 'E4', 'A4'], name: 'Ля-диез полууменьшенный септаккорд' },
+    'Bbm7b5': { notes: ['Bb3', 'Db4', 'F4', 'Ab4'], name: 'Си-бемоль полууменьшенный септаккорд' },
+    'Bm7b5':  { notes: ['B3', 'D4', 'F4', 'A4'], name: 'Си полууменьшенный септаккорд' },
+    'B#m7b5': { notes: ['C4', 'Eb4', 'Gb4', 'Bb4'], name: 'Си-диез полууменьшенный септаккорд' },
+    
+    // Augmented 7th (aug7, +7) - увеличенные септаккорды: 1-3-#5-b7
+    'Caug7':  { notes: ['C3', 'E3', 'G#3', 'Bb3'], name: 'До увеличенный септаккорд' },
+    'C#aug7': { notes: ['C#3', 'F3', 'A3', 'B3'], name: 'До-диез увеличенный септаккорд' },
+    'Cbaug7': { notes: ['B2', 'D#3', 'G3', 'A3'], name: 'До-бемоль увеличенный септаккорд' },
+    'Dbaug7': { notes: ['Db3', 'F3', 'A3', 'C4'], name: 'Ре-бемоль увеличенный септаккорд' },
+    'Daug7':  { notes: ['D3', 'F#3', 'A#3', 'C4'], name: 'Ре увеличенный септаккорд' },
+    'D#aug7': { notes: ['D#3', 'G3', 'B3', 'C#4'], name: 'Ре-диез увеличенный септаккорд' },
+    'Ebaug7': { notes: ['Eb3', 'G3', 'B3', 'Db4'], name: 'Ми-бемоль увеличенный септаккорд' },
+    'Eaug7':  { notes: ['E3', 'G#3', 'C4', 'D4'], name: 'Ми увеличенный септаккорд' },
+    'E#aug7': { notes: ['F3', 'A3', 'C#4', 'Eb4'], name: 'Ми-диез увеличенный септаккорд' },
+    'Faug7':  { notes: ['F3', 'A3', 'C#4', 'Eb4'], name: 'Фа увеличенный септаккорд' },
+    'Fbaug7': { notes: ['E3', 'G#3', 'C4', 'D4'], name: 'Фа-бемоль увеличенный септаккорд' },
+    'F#aug7': { notes: ['F#3', 'A#3', 'D4', 'E4'], name: 'Фа-диез увеличенный септаккорд' },
+    'Gbaug7': { notes: ['Gb3', 'Bb3', 'D4', 'F4'], name: 'Соль-бемоль увеличенный септаккорд' },
+    'Gaug7':  { notes: ['G3', 'B3', 'D#4', 'F4'], name: 'Соль увеличенный септаккорд' },
+    'G#aug7': { notes: ['G#3', 'C4', 'E4', 'G4'], name: 'Соль-диез увеличенный септаккорд' },
+    'Abaug7': { notes: ['Ab3', 'C4', 'E4', 'Gb4'], name: 'Ля-бемоль увеличенный септаккорд' },
+    'Aaug7':  { notes: ['A3', 'C#4', 'F4', 'G4'], name: 'Ля увеличенный септаккорд' },
+    'A#aug7': { notes: ['A#3', 'D4', 'F#4', 'A4'], name: 'Ля-диез увеличенный септаккорд' },
+    'Bbaug7': { notes: ['Bb3', 'D4', 'F#4', 'Ab4'], name: 'Си-бемоль увеличенный септаккорд' },
+    'Baug7':  { notes: ['B3', 'D#4', 'G4', 'A4'], name: 'Си увеличенный септаккорд' },
+    'B#aug7': { notes: ['C4', 'E4', 'G#4', 'Bb4'], name: 'Си-диез увеличенный септаккорд' }
 };
 
 // Парсинг пользовательских аккордов
@@ -187,8 +329,29 @@ export function midiToFrequency(midiNote) {
     return 440 * Math.pow(2, (midiNote - 69) / 12);
 }
 
-// Воспроизведение одной ноты
-export function playNote(frequency, duration, startTime, audioContext) {
+// Параметры синтезатора для плавного звука
+// Можно изменить эти параметры для настройки звучания:
+// - waveType: 'sine' (чистый), 'triangle' (мягкий), 'sawtooth' (яркий), 'square' (резкий)
+// - attackTime: время атаки (меньше = быстрее начало, но возможны щелчки)
+// - sustainLevel: уровень sustain (0.0-1.0, выше = громче равномерный звук)
+// - releaseTime: время затухания в конце (больше = плавнее окончание)
+export const synthSettings = {
+    // Тип волны: 'sine', 'triangle', 'sawtooth', 'square'
+    // 'sine' - самый чистый, 'triangle' - мягче, 'sawtooth' - ярче
+    waveType: 'sine',
+    
+    // Параметры огибающей ADSR (Attack, Decay, Sustain, Release)
+    attackTime: 0.001,    // Очень быстрая атака (почти без щелчков)
+    decayTime: 0.01,      // Короткий спад до уровня sustain
+    sustainLevel: 0.8,    // Уровень sustain (равномерный звук, увеличен для более тянучего звука)
+    releaseTime: 0.05,    // Плавное затухание в конце
+    
+    // Общая громкость
+    volume: 0.25
+};
+
+// Воспроизведение одной ноты с улучшенной огибающей
+export function playNote(frequency, duration, startTime, audioContext, settings = synthSettings) {
     const oscillator = audioContext.createOscillator();
     const gainNode = audioContext.createGain();
     
@@ -196,29 +359,105 @@ export function playNote(frequency, duration, startTime, audioContext) {
     gainNode.connect(audioContext.destination);
     
     oscillator.frequency.value = frequency;
-    oscillator.type = 'sine';
+    oscillator.type = settings.waveType;
     
-    // Огибающая для плавного звука
-    const now = audioContext.currentTime;
+    // Улучшенная огибающая ADSR для плавного и тянучего звука
+    const now = startTime;
+    const attackEnd = now + settings.attackTime;
+    const decayEnd = attackEnd + settings.decayTime;
+    const releaseStart = now + duration - settings.releaseTime;
+    
+    // Attack: плавный подъем от 0 до пика
     gainNode.gain.setValueAtTime(0, now);
-    gainNode.gain.linearRampToValueAtTime(0.3, now + 0.01);
-    gainNode.gain.exponentialRampToValueAtTime(0.01, now + duration);
+    gainNode.gain.linearRampToValueAtTime(settings.volume * 1.2, attackEnd);
+    
+    // Decay: спад до уровня sustain
+    gainNode.gain.linearRampToValueAtTime(settings.volume * settings.sustainLevel, decayEnd);
+    
+    // Sustain: удерживаем постоянный уровень на протяжении большей части длительности
+    gainNode.gain.setValueAtTime(settings.volume * settings.sustainLevel, decayEnd);
+    
+    // Release: плавное затухание только в конце
+    if (releaseStart > decayEnd) {
+        gainNode.gain.setValueAtTime(settings.volume * settings.sustainLevel, releaseStart);
+        gainNode.gain.exponentialRampToValueAtTime(0.001, now + duration);
+    } else {
+        // Если длительность слишком короткая, просто плавно затухаем
+        gainNode.gain.exponentialRampToValueAtTime(0.001, now + duration);
+    }
     
     oscillator.start(startTime);
     oscillator.stop(startTime + duration);
 }
 
 // Воспроизведение аккорда
-export function playChord(chordInfo, startTime, audioContext, duration) {
+export function playChord(chordInfo, startTime, audioContext, duration, settings = synthSettings) {
     if (!chordInfo || !chordInfo.notes) return;
     
     chordInfo.notes.forEach(note => {
         const midiNote = midiNotes[note];
         if (midiNote !== undefined) {
             const frequency = midiToFrequency(midiNote);
-            playNote(frequency, duration, startTime, audioContext);
+            playNote(frequency, duration, startTime, audioContext, settings);
         }
     });
+}
+
+// Опциональная функция для более богатого тембра (несколько осцилляторов)
+// Можно использовать вместо playNote для более насыщенного звука
+export function playNoteRich(frequency, duration, startTime, audioContext, settings = synthSettings) {
+    // Основной осциллятор
+    const oscillator1 = audioContext.createOscillator();
+    const gainNode1 = audioContext.createGain();
+    
+    oscillator1.connect(gainNode1);
+    gainNode1.connect(audioContext.destination);
+    
+    oscillator1.frequency.value = frequency;
+    oscillator1.type = settings.waveType;
+    
+    // Опционально: добавить октаву выше для яркости (можно закомментировать)
+    const oscillator2 = audioContext.createOscillator();
+    const gainNode2 = audioContext.createGain();
+    
+    oscillator2.connect(gainNode2);
+    gainNode2.connect(audioContext.destination);
+    
+    oscillator2.frequency.value = frequency * 2; // Октава выше
+    oscillator2.type = settings.waveType;
+    
+    // Огибающая для основного осциллятора
+    const now = startTime;
+    const attackEnd = now + settings.attackTime;
+    const decayEnd = attackEnd + settings.decayTime;
+    const releaseStart = now + duration - settings.releaseTime;
+    
+    // Основной осциллятор (громче)
+    gainNode1.gain.setValueAtTime(0, now);
+    gainNode1.gain.linearRampToValueAtTime(settings.volume * 1.2, attackEnd);
+    gainNode1.gain.linearRampToValueAtTime(settings.volume * settings.sustainLevel, decayEnd);
+    gainNode1.gain.setValueAtTime(settings.volume * settings.sustainLevel, decayEnd);
+    
+    // Второй осциллятор (тише, для обертонов)
+    gainNode2.gain.setValueAtTime(0, now);
+    gainNode2.gain.linearRampToValueAtTime(settings.volume * 0.3, attackEnd);
+    gainNode2.gain.linearRampToValueAtTime(settings.volume * 0.15, decayEnd);
+    gainNode2.gain.setValueAtTime(settings.volume * 0.15, decayEnd);
+    
+    if (releaseStart > decayEnd) {
+        gainNode1.gain.setValueAtTime(settings.volume * settings.sustainLevel, releaseStart);
+        gainNode1.gain.exponentialRampToValueAtTime(0.001, now + duration);
+        gainNode2.gain.setValueAtTime(settings.volume * 0.15, releaseStart);
+        gainNode2.gain.exponentialRampToValueAtTime(0.001, now + duration);
+    } else {
+        gainNode1.gain.exponentialRampToValueAtTime(0.001, now + duration);
+        gainNode2.gain.exponentialRampToValueAtTime(0.001, now + duration);
+    }
+    
+    oscillator1.start(startTime);
+    oscillator1.stop(startTime + duration);
+    oscillator2.start(startTime);
+    oscillator2.stop(startTime + duration);
 }
 
 // Форматирование времени
